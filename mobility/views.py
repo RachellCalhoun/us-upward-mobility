9482ca1ca1b442d14b00bfdf1739e30223b38251
Rachell Calhoun <rachell.calhoun@gmail.com>
2022-08-03 14:15:57 -0400

    add plotly interactive

--- mobility/views.py
+++ mobility/views.py
@@ -4,7 +4,11 @@ from django.template import loader
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-
+import pandas as pd
+import plotly.offline as opy
+from urllib.request import urlopen
+import json
+import plotly.express as px
 
 from mobility.models import Post
 # def index(request):
@@ -28,11 +32,29 @@ def compare_counties(request):
 
 def national_view(request):
     template = loader.get_template('mobility/national_view.html')
-    context = {
-        'filler': 'latest_question_list',
-    }
-    return HttpResponse(template.render(context, request))
+    # plotly example
+    f = open('geojson-counties-fips.json')
+    # returns JSON object as
+    # a dictionary
+    counties = json.load(f)
 
+    df = pd.read_csv("counties_merged.csv",
+                    dtype={"fips": str})
+
+    fig2 = px.choropleth(df, geojson=counties, locations='FIPS', color='proportion_homeless',
+                            color_continuous_scale="Viridis",
+                            range_color=(0, 1),
+                            scope="usa",
+                            labels={'proportion_homeless':'proportion_homeless'}
+                                )
+    print('fig done')
+    fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+    print('fig update done')
+    pl_div = opy.plot(fig2, output_type='div', include_plotlyjs=False)
+    print('create plt')
+    context = { 'pl_div': pl_div}
+    return HttpResponse(template.render(context, request))
 
 class PostListView(ListView):
     model = Post
