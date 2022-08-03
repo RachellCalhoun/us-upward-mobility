9a728437ca6ef1914b7ddfd8546df16707ea90cd
Rachell Calhoun <rachell.calhoun@gmail.com>
2022-08-03 18:19:55 -0400

    add form metrics

--- mobility/views.py
+++ mobility/views.py
@@ -8,11 +8,13 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.conf import settings
 
+
 import pandas as pd
 import plotly.offline as opy
 import plotly.express as px
 
 from mobility.models import Post
+from mobility.forms import MetricForm
 
 def index(request):
     template = loader.get_template('mobility/index.html')
@@ -29,8 +31,9 @@ def compare_counties(request):
 
 
 def national_view(request):
-    metric = request.GET.get('metric', None)
 
+    metric = request.GET.get('metric', None)
+    metric_form = MetricForm(initial={'metric': request.GET.get('metric', None)})
     template = loader.get_template('mobility/national_view.html')
     fips = request.GET.get('fips', None)
     context = {'fips': fips, 'metric': metric}
@@ -48,16 +51,18 @@ def national_view(request):
     df = pd.read_csv(f"{file_path}counties_merged.csv",
                     dtype={"fips": str})
     df['FIPS'] = df['FIPS'].astype(int).astype(str).str.zfill(5)
+    low = min(df[metric].values)
+    high = max(df[metric].values)
     fig2 = px.choropleth(df, geojson=counties, locations='FIPS', color=metric,
                             color_continuous_scale="Viridis",
-                            range_color=(0, 1),
+                            range_color=(low, high),
                             scope="usa",
                             labels={metric:metric}
                                 )
     fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     pl_div = opy.plot(fig2, output_type='div', include_plotlyjs=False)
 
-    context = { 'pl_div': pl_div}
+    context = { 'pl_div': pl_div, 'form': metric_form}
     return HttpResponse(template.render(context, request))
 
 class PostListView(ListView):
