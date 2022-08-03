55eb76508957aba2f586825265f3e71c3825c2f4
Rachell Calhoun <rachell.calhoun@gmail.com>
2022-08-03 19:54:53 -0400

    add county selector

--- mobility/forms.py
+++ mobility/forms.py
@@ -1,85 +1,11 @@
 
 from secrets import choice
 from django import forms
+from .choices_lists import METRIC_CHOICES, COUNTY_CHOICES
 
 class MetricForm(forms.Form):
-
-    METRIC_CHOICES = (
-        ('proportion_homeless', 'Proportion Homeless'),
-        ('HPSA Score', 'HPSA Score'),
-        ('proportion_voter', 'Proportion Voter'),
-        ('proportion_high_poverty_neighborhood',
-         'Proportion High Poverty Neighborhood'),
-
-        ('median_family_income', 'Median Family Income'),
-        ('income_20_percentile', 'Income 20 Percentile'),
-        ('income_80_percentile', 'Income 80 Percentile'),
-        ('median_family_income_white', 'Median Family Income White'),
-        ('median_family_income_black', 'Median Family Income Black'),
-        ('median_family_income_indigenous', 'Median Family Income Indigenous'),
-        ('median_family_income_asian', 'Median Family Income Asian'),
-        ('median_family_income_hispanic', 'Median Family Income Hispanic'),
-
-        ('low_birth_rate', 'Low Birth Rate'),
-        ('Not Hispanic or Latino_low_birth_rate',
-         'Not Hispanic Or Latino Low Birth Rate'),
-        ('Hispanic or Latino_low_birth_rate',
-         'Hispanic Or Latino Low Birth Rate'),
-        ('Unknown or Not Stated_low_birth_rate',
-         'Unknown Or Not Stated Low Birth Rate'),
-        ('Black or African American_low_birth_rate',
-         'Black Or African American Low Birth Rate'),
-        ('White_low_birth_rate', 'White Low Birth Rate'),
-        ('Asian_low_birth_rate', 'Asian Low Birth Rate'),
-        ('More than one race_low_birth_rate',
-         'More Than One Race Low Birth Rate'),
-        ('American Indian or Alaska Native_low_birth_rate',
-         'American Indian Or Alaska Native Low Birth Rate'),
-        ('Native Hawaiian or Other Pacific Islander_low_birth_rate',
-         'Native Hawaiian Or Other Pacific Islander Low Birth Rate'),
-
-        ('hispanic_or_latino_exposure', 'Hispanic Or Latino Exposure'),
-        ('white_exposure', 'White Exposure'),
-        ('black_exposure', 'Black Exposure'),
-        ('native_american_exposure', 'Native American Exposure'),
-        ('Asian Exposure', 'asian_exposure'),
-        ('hawaiian_exposure', 'Hawaiian Exposure'),
-        ('some_other_race_alone_exposure',
-         'Some Other Race Alone Exposure'),
-        ('two_more_races_exposure', 'Two More Races Exposure'),
-
-        ('transit_trips_index', 'Transit Trips Index'),
-        ('transit_low_cost_index', 'Transit Low Cost Index'),
-        ('crime_rate', 'Crime Rate'),
-
-        ('juvenile_crime_rate', 'Juvenile Crime Rate'),
-        ('avg_edu_prof_diff', 'Avg Edu Prof Diff'),
-
-        ('preschool_enroll', 'Preschool Enroll'),
-        ('white_under_5', 'White Under 5'),
-        ('black_under_5', 'Black Under 5'),
-        ('indigenous_under_5', 'Indigenous Under 5'),
-        ('asian_under_5', 'Asian Under 5'),
-        ('hispanic_under_5', 'Hispanic Under 5'),
-        ('two_or_more_race_under_5', 'Two Or More Race Under 5'),
-        ('some_other_race_under_5', 'Some Other Race Under 5'),
-        ('preschool_enrollment_white', 'Preschool Enrollment White'),
-        ('preschool_enrollment_black', 'Preschool Enrollment Black'),
-        ('preschool_enrollment_hispanic', 'Preschool Enrollment Hispanic'),
-        ('preschool_enrollment_indigenous', 'Preschool Enrollment Indigenous'),
-        ('preschool_enrollment_asian', 'Preschool Enrollment Asian'),
-
-        ('population', 'Population'),
-        ('white_employed_16_64', 'White Employed 16 64'),
-        ('black_employed_16_64', 'Black Employed 16 64'),
-        ('american_indian_employed_16_64', 'American Indian Employed 16 64'),
-        ('asian_employed_16_64', 'Asian Employed 16 64' ),
-        ('some_other_race_alone_employed_16_64', 'Some Other Race Alone Employed 16 64'),
-        ('two_or_more_race_employed_16_64', 'Two Or More Race Employed 16 64'),
-        ('hispanic_or_latino_employed_16_64', 'Hispanic Or Latino Employed 16 64'),
-        ('employed_25_54_population', 'Employed 25 54 Population'),
-        ('employed_16_64_population', 'Employed 16 64 Population')
-
-    )
-
     metric = forms.ChoiceField(choices=METRIC_CHOICES)
+
+
+class CountyForm(forms.Form):
+    fips = forms.ChoiceField(choices=COUNTY_CHOICES, label="County")
--- mobility/views.py
+++ mobility/views.py
@@ -8,13 +8,12 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.conf import settings
 
-
 import pandas as pd
 import plotly.offline as opy
 import plotly.express as px
 
 from mobility.models import Post
-from mobility.forms import MetricForm
+from mobility.forms import MetricForm, CountyForm
 
 def index(request):
     template = loader.get_template('mobility/index.html')
@@ -25,24 +24,27 @@ def index(request):
 
 def compare_counties(request):
     fips = request.GET.get('fips', None)
-    context = {'fips': fips}
+    form = CountyForm(initial={'fips': request.GET.get('fips', None)})
+    context = {'fips': fips, 'form': form}
     template = loader.get_template('mobility/compare_counties.html')
     return HttpResponse(template.render(context, request))
 
 
 def national_view(request):
 
-    metric = request.GET.get('metric', None)
-    metric_form = MetricForm(initial={'metric': request.GET.get('metric', None)})
+    metric = request.GET.get('metric', 'population')
+    metric_form = MetricForm(initial={'metric': request.GET.get('metric', 'population')})
     template = loader.get_template('mobility/national_view.html')
     fips = request.GET.get('fips', None)
     context = {'fips': fips, 'metric': metric}
     # plotly example
     if settings.DEBUG:
         file_path = ''
+        url = ''
 
     else:
         file_path = '/home/upwardmobility/upwardmobility.pythonanywhere.com/'
+        url = "https://upwardmobility.pythonanywhere.com/compare-counties"
     f = open(f'{file_path}geojson-counties-fips.json')
     # returns JSON object as
     # a dictionary
@@ -50,15 +52,31 @@ def national_view(request):
 
     df = pd.read_csv(f"{file_path}counties_merged.csv",
                     dtype={"fips": str})
+
     df['FIPS'] = df['FIPS'].astype(int).astype(str).str.zfill(5)
-    low = min(df[metric].values)
-    high = max(df[metric].values)
+
+    df['LINK'] = "[Link](https://upwardmobility.pythonanywhere.com/compare-counties?fips=" + df['FIPS'] + ")"
+    if metric:
+        low = min(df[metric].values)
+        high = max(df[metric].values)
+    else:
+        low = 0
+        high = 1
     fig2 = px.choropleth(df, geojson=counties, locations='FIPS', color=metric,
                             color_continuous_scale="Viridis",
                             range_color=(low, high),
                             scope="usa",
-                            labels={metric:metric}
-                                )
+                            labels={metric:metric},hover_data=["NAME", metric, "LINK"])
+
+    fig2.update_layout(
+        hoverlabel=dict(
+            bgcolor="white",
+            font_size=16,
+            font_family="Rockwell",
+        )
+    )
+
+
     fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
     pl_div = opy.plot(fig2, output_type='div', include_plotlyjs=False)
 
