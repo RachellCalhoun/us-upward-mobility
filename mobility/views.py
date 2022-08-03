f6ef1ce9383da39b3ff79715da35c137384782fc
Rachell Calhoun <rachell.calhoun@gmail.com>
2022-08-03 15:30:46 -0400

    iframe initial

--- mobility/views.py
+++ mobility/views.py
@@ -23,15 +23,19 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def compare_counties(request):
+    fips = request.GET.get('fips', None)
+    context = {'fips': fips}
     template = loader.get_template('mobility/compare_counties.html')
-    context = {
-        'filler': 'latest_question_list',
-    }
     return HttpResponse(template.render(context, request))
 
 
 def national_view(request):
+    metric = request.GET.get('metric', None)
+
+
     template = loader.get_template('mobility/national_view.html')
+    fips = request.GET.get('fips', None)
+    context = {'fips': fips, 'metric': metric}
     # plotly example
     f = open('geojson-counties-fips.json')
     # returns JSON object as
@@ -41,18 +45,15 @@ def national_view(request):
     df = pd.read_csv("counties_merged.csv",
                     dtype={"fips": str})
 
-    fig2 = px.choropleth(df, geojson=counties, locations='FIPS', color='proportion_homeless',
+    fig2 = px.choropleth(df, geojson=counties, locations='FIPS', color=metric,
                             color_continuous_scale="Viridis",
                             range_color=(0, 1),
                             scope="usa",
-                            labels={'proportion_homeless':'proportion_homeless'}
+                            labels={metric:metric}
                                 )
-    print('fig done')
     fig2.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-    print('fig update done')
     pl_div = opy.plot(fig2, output_type='div', include_plotlyjs=False)
-    print('create plt')
+
     context = { 'pl_div': pl_div}
     return HttpResponse(template.render(context, request))
 
