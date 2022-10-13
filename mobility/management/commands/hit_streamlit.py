dcefa3910fe22e22c375bab676c4d96e6d0a5c9a
Rachell Calhoun <rachell.calhoun@gmail.com>
2022-10-13 14:11:59 -0400

    add management command to hit streamlit

---
+++ mobility/management/commands/hit_streamlit.py
@@ -0,0 +1,16 @@
+
+import logging
+import requests as re
+
+from django.core.management.base import BaseCommand
+
+
+
+logger = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    """"""
+    def handle(self, *args, **options):
+
+        streamlit_response = re.get('https://rachellcalhoun-citizenlabs-upwardmobili-streamlitappmain-2n5268.streamlitapp.com/')
+        self.stdout.write(f'Script finished runnning with {streamlit_response}')
